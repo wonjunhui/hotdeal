4a21b78f44a1bcecbd2d81a7da7eebe9b3f42bcf
KwakSeungHyeok <league3236@gmail.com>
2019-03-10 11:25:00 +0900

    루리웹 크롤링 중..(서버좀 만들어주세요 형님들)

---
+++ ruliwebCraw.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import urllib.request
+from bs4 import BeautifulSoup
+import json
+from collections import OrderedDict
+import re
+
+basic_url = "http://bbs.ruliweb.com/market/board/1020/read/25415"
+
+fp = urllib.request.urlopen(basic_url)
+source = fp.read()
+fp.close()
+
+soup = BeautifulSoup(source, 'html.parser')
+title = soup.find("span", class_="subject_text")
+title = title.get_text()
+
+link = soup.find("div", class_="source_url")
+link = link.find("a")
+link = link.get_text()
+
+images = []
+img = soup.find_all("a", class_="img_load")
+print(img)
+# img = img[2]
+# for imged in img.find_all("img"):
+#     images.append("http:"+imged.get("src"))
+#
+# print(basic_url)
+# print(title)
+# print(link)
+# print(images)
